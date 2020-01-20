d42ee215657fb46e509a7303c5f2088fa4784e43
Changbeom Choi <cbchoi@handong.edu>
2020-01-20 22:33:51 +0900

    example added

---
+++ pohangsim/hierarachical_test/config.py
@@ -0,0 +1,22 @@
+# Stores credential information
+GIT_USER_ID='test'
+GIT_USER_PASSWORD='test'
+
+
+# Google Drive API, credentials
+GOOGLE_SERVICE_KEY='Your-Google-service-key'
+GOOGLE_SPREADSHEET_NAME = 'Your-Google-spreadsheet-name'
+GOOGLE_WORKSHEET = 'Active-Worksheet'
+
+# PUBLIC_DATA_SERVICE_KEY
+PUBLIC_DATA_SERVICE_KEY = "Your-Public-Data-Service-Key"
+PUBLIC_DATA_REQUEST_URL = "Your-data-request-url"
+
+# Air Pollution
+STATION = "Station-name"
+
+# SIMULATION_MODE
+SIMULATION_MODE = 'VIRTUAL_TIME'
+
+# SIMULATION_MODEL
+REQUEST_FREQ = 1
