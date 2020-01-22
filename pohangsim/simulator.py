15fcbcbebb94afff5e0f8e7c90a91b6f7ea1706e
cbchoi <cbchoi@handong.edu>
2020-01-22 23:29:14 +0900

    blue3mtest

--- pohangsim/simulator.py
+++ pohangsim/simulator.py
@@ -4,7 +4,7 @@ import random
 for i in range(1,31):
 	random.seed(i)
 	print(i)
-	EXPORT204 = "python3 building_test.py > onlyblue"+str(i)+".log"    #EXPORT 설정
+	EXPORT204 = "python3 building_test.py > blue_3m_"+str(i)+".log"    #EXPORT 설정
 	#xx= "python3 average_satisfaction.py"
 	os.system(EXPORT204) 
 	#os.system(xx)
