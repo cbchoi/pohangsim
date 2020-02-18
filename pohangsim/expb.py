a0c5f0be938bd6acf5da0fee652194d4dcfbffd9
youpeterabb1t <21300259@handong.edu>
2020-02-18 23:22:56 +0900

    auto mkdir update

--- pohangsim/expb.py
+++ pohangsim/expb.py
@@ -1,5 +1,5 @@
 import contexts
-import sys
+import sys,os
 #import numpy as np
 
 from evsim.system_simulator import SystemSimulator
@@ -28,8 +28,8 @@ from government import Government
 from garbagecan import GarbageCan
 from garbage_truck import GarbageTruck
 from family import Family
-#simulation_time=448
-simulation_time=2192 #quarter
+simulation_time=448
+#simulation_time=2192 #quarter
 #simulation_time=8762 # year
 #simulation_time=26282# 3year
 #simulation_time=43802# 5year
@@ -38,6 +38,11 @@ simulation_time=2192 #quarter
 blist=[]
 hlist=[]
 fam=[]
+memo=str(sys.argv[1])
+stddev=str(sys.argv[2])
+if not os.path.exists(memo+stddev):
+    os.makedirs(memo+stddev)
+
 
 #file = open('half.txt','r')
 file = open('update/population_b100_N99_seed0.txt','r')
@@ -63,7 +68,7 @@ SystemSimulator().register_engine("sname", SIMULATION_MODE, TIME_DENSITY)
 
 c = Clock(0, simulation_time, "clock", "sname")
 SystemSimulator().get_engine("sname").register_entity(c)
-gt = GarbageTruck(0, simulation_time, "garbage_truck", 'sname', 68, [e for e in enumerate([0.1 for building in blist])],"7am_only_b0.4")#4.7*13*3
+gt = GarbageTruck(0, simulation_time, "garbage_truck", 'sname', 68, [e for e in enumerate([0.1 for building in blist])],memo+stddev)#4.7*13*3
 SystemSimulator().get_engine("sname").register_entity(gt)
 
 gv = Government(0, simulation_time,"government","sname")
@@ -87,7 +92,7 @@ j=0
 for building in blist:
 
 
-    g = GarbageCan(0, simulation_time, "gc[{0}]".format(i), 'sname', 55,"7am_only_b0.4")
+    g = GarbageCan(0, simulation_time, "gc[{0}]".format(i), 'sname', 55,memo+stddev)
     SystemSimulator().get_engine("sname").register_entity(g)
     
     for flist in building:
---
+++ pohangsim/test.py
@@ -0,0 +1,4 @@
+import os
+memo = "student"
+stddev=str(0.2)
+os.system("mkdir "+memo+stddev+"")
