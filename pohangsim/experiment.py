c637b5f5e454f95afc7d38594b83ef828b2914b2
chlyoo <changhyunlyoo@gmail.com>
2020-02-19 20:20:56 +0900

    source update

--- pohangsim/experiment.py
+++ pohangsim/experiment.py
@@ -1,11 +1,31 @@
 import os
-from config import TIME_STDDEV
-exp1 = "python3 exp.py housewife bhs_N100_seed0  > blue_hs_"+str(TIME_STDDEV)+".log"
-exp2 = "python3 exp.py housewife bsh_N100_seed0  > blue_sh_"+str(TIME_STDDEV)+".log"
-exp3 = "python3 exp.py housewife sbh_N100_seed0  > stud_bh_"+str(TIME_STDDEV)+".log"
-exp4 = "python3 exp.py housewife shb_N100_seed0  > stud_hb_"+str(TIME_STDDEV)+".log"
-exp5 = "python3 exp.py housewife hsb_N100_seed0  > house_sb_"+str(TIME_STDDEV)+".log"
-exp6 = "python3 exp.py housewife hbs_N100_seed0  > house_bs_"+str(TIME_STDDEV)+".log"
+from config import *
+#532
+exp1 = "python3 exp.py housewife bhs532_N100_seed0  > 532blue_hs_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)".log"
+exp2 = "python3 exp.py housewife bsh532_N100_seed0  > 532blue_sh_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)".log"
+exp3 = "python3 exp.py housewife sbh532_N100_seed0  > 532stud_bh_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)".log"
+exp4 = "python3 exp.py housewife shb532_N100_seed0  > 532stud_hb_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)".log"
+exp5 = "python3 exp.py housewife hsb532_N100_seed0  > 532house_sb_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)".log"
+exp6 = "python3 exp.py housewife hbs532_N100_seed0  > 532house_bs_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)".log"
+#442
+exp7 = "python3 exp.py housewife bhs532_N100_seed0  > 442blue_hs_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)".log"
+exp8 = "python3 exp.py housewife bsh532_N100_seed0  > 442blue_sh_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)".log"
+exp9 = "python3 exp.py housewife sbh532_N100_seed0  > 442stud_bh_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)".log"
+#721
+exp10 = "python3 exp.py housewife bhs721_N100_seed0  > 721blue_hs_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)".log"
+exp11 = "python3 exp.py housewife bsh721_N100_seed0  > 721blue_sh_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)".log"
+exp12 = "python3 exp.py housewife sbh721_N100_seed0  > 721stud_bh_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)".log"
+exp10 = "python3 exp.py housewife shb721_N100_seed0  > 721stud_hb_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)".log"
+exp11 = "python3 exp.py housewife hsb721_N100_seed0  > 721house_sb_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)".log"
+exp12 = "python3 exp.py housewife hbs721_N100_seed0  > 721house_bs_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)".log"
+
+
+
+
+
+
+
+
 os.system(exp1)
 os.system(exp2)
 os.system(exp3)
