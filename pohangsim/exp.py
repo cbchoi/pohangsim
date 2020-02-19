c00a6ab94f4048cade619c1a18ba43474a1e1cda
cbchoi <cbchoi@handong.edu>
2020-02-19 14:33:35 +0000

    experiment update

--- pohangsim/exp.py
+++ pohangsim/exp.py
@@ -21,105 +21,106 @@ from garbagecan import GarbageCan
 from garbage_truck import GarbageTruck
 from family import Family
 
-blist=[]
-hlist=[]
-fam=[]
-outputlocation=str(sys.argv[1])+str(TIME_STDDEV)
-if not os.path.exists(outputlocation):
-    os.makedirs(outputlocation)
+for kndx in range(30):
+    blist=[]
+    hlist=[]
+    fam=[]
+    outputlocation=str(sys.argv[1])+str(TIME_STDDEV)
+    if not os.path.exists(outputlocation):
+        os.makedirs(outputlocation)
 
-file = open("update/"+sys.argv[2]+".txt",'r')
-lines = file.readlines()
-file.close()
-for i in range(len(lines)):  
-    line = lines[i].split('\n')[0]
-    if not line == "": #빈칸이 아닐경우
-        elements = (line.split(','))
-        for j in elements: #패밀리 안의 멤버=j
-            fam.append(eval(j)) #j를 fam추가
-        hlist.append(fam) #fam을 hlist에 추가
-        fam=[]    
-    else:
-        blist.append(hlist)
-        hlist = []
-    if i == len(lines)-1:
-        blist.append(hlist)
+    file = open("update/"+sys.argv[2]+".txt",'r')
+    lines = file.readlines()
+    file.close()
+    for i in range(len(lines)):  
+        line = lines[i].split('\n')[0]
+        if not line == "": #빈칸이 아닐경우
+            elements = (line.split(','))
+            for j in elements: #패밀리 안의 멤버=j
+                fam.append(eval(j)) #j를 fam추가
+            hlist.append(fam) #fam을 hlist에 추가
+            fam=[]    
+        else:
+            blist.append(hlist)
+            hlist = []
+        if i == len(lines)-1:
+            blist.append(hlist)
 
-se = SystemSimulator()
+    se = SystemSimulator()
 
-se.register_engine("sname", SIMULATION_MODE, TIME_DENSITY)
+    se.register_engine("sname", SIMULATION_MODE, TIME_DENSITY)
 
-c = Clock(0, simulation_time, "clock", "sname")
-se.get_engine("sname").register_entity(c)
-gt = GarbageTruck(0, simulation_time, "garbage_truck", 'sname', GARBAGETRUCK_SIZE, [e for e in enumerate([TRUCK_DELAY for building in blist])],outputlocation)#4.7*13*3
-se.get_engine("sname").register_entity(gt)
+    c = Clock(0, simulation_time, "clock", "sname")
+    se.get_engine("sname").register_entity(c)
+    gt = GarbageTruck(0, simulation_time, "garbage_truck", 'sname', GARBAGETRUCK_SIZE, [e for e in enumerate([TRUCK_DELAY for building in blist])],outputlocation)#4.7*13*3
+    se.get_engine("sname").register_entity(gt)
 
-gv = Government(0, simulation_time,"government","sname")
-se.get_engine("sname").register_entity(gv)
+    gv = Government(0, simulation_time,"government","sname")
+    se.get_engine("sname").register_entity(gv)
 
-#Building Register
+    #Building Register
 
-i=0
-j=0
-for building in blist:
+    i=0
+    j=0
+    for building in blist:
 
 
-    g = GarbageCan(0, simulation_time, "gc[{0}]".format(i), 'sname', GARBAGECAN_SIZE,outputlocation)
-    se.get_engine("sname").register_entity(g)
-    
-    for flist in building:
-        ftype = FamilyType(TEMP_CAN_SIZE)
-        f = Family(0, simulation_time,"family",'sname', ftype)
-        for htype in flist:
-            #hid = get_human_id()
-            name = htype.get_name()
-            cname = "check[{0}]".format(htype.get_name())               
-            h1 = Human(0, simulation_time, cname, "sname", htype)
-            ch = Check(0, simulation_time, name, "sname", htype)
+        g = GarbageCan(0, simulation_time, "gc[{0}]".format(i), 'sname', GARBAGECAN_SIZE,outputlocation)
+        se.get_engine("sname").register_entity(g)
+        
+        for flist in building:
+            ftype = FamilyType(TEMP_CAN_SIZE)
+            f = Family(0, simulation_time,"family",'sname', ftype)
+            for htype in flist:
+                #hid = get_human_id()
+                name = htype.get_name()
+                cname = "check[{0}]".format(htype.get_name())               
+                h1 = Human(0, simulation_time, cname, "sname", htype)
+                ch = Check(0, simulation_time, name, "sname", htype)
 
-            se.get_engine("sname").register_entity(h1)
-            se.get_engine("sname").register_entity(ch)
-            #f1.register_member(htype)
-            ftype.register_member(htype)
-            
-            # Connect Check & Can
-            ports = g.register_human(htype.get_id())
-            se.get_engine("sname").coupling_relation(h1, "trash", ch, "request")
-            se.get_engine("sname").coupling_relation(ch, "check", g, ports[0])
+                se.get_engine("sname").register_entity(h1)
+                se.get_engine("sname").register_entity(ch)
+                #f1.register_member(htype)
+                ftype.register_member(htype)
+                
+                # Connect Check & Can
+                ports = g.register_human(htype.get_id())
+                se.get_engine("sname").coupling_relation(h1, "trash", ch, "request")
+                se.get_engine("sname").coupling_relation(ch, "check", g, ports[0])
 
-            se.get_engine("sname").coupling_relation(g, ports[1], ch, "checked")
-            se.get_engine("sname").coupling_relation(ch, "gov_report", gv, "recv_report")
-    
-            se.get_engine("sname").coupling_relation(None, "start", h1, "start")
-            se.get_engine("sname").coupling_relation(None, "end", h1, "end")
-            se.get_engine("sname").coupling_relation(h1, "trash", f, "receive_membertrash")
-       
+                se.get_engine("sname").coupling_relation(g, ports[1], ch, "checked")
+                se.get_engine("sname").coupling_relation(ch, "gov_report", gv, "recv_report")
+        
+                se.get_engine("sname").coupling_relation(None, "start", h1, "start")
+                se.get_engine("sname").coupling_relation(None, "end", h1, "end")
+                se.get_engine("sname").coupling_relation(h1, "trash", f, "receive_membertrash")
+           
 
-        se.get_engine("sname").register_entity(f)
+            se.get_engine("sname").register_entity(f)
 
-        ports = g.register_family(j)
-        se.get_engine("sname").coupling_relation(f, "takeout_trash", g, ports[0])
-        j+=1
+            ports = g.register_family(j)
+            se.get_engine("sname").coupling_relation(f, "takeout_trash", g, ports[0])
+            j+=1
+
+        # Connect Truck & Can
+        ports = gt.register_garbage_can(i)
+        se.get_engine("sname").coupling_relation(g, "res_garbage", gt, ports[0])
+        se.get_engine("sname").coupling_relation(gt, ports[1], g, "req_empty")
+        i+=1
+
+    se.get_engine("sname").insert_input_port("start")
+
+
+    se.get_engine("sname").coupling_relation(None, "start", c, "start")
+    se.get_engine("sname").coupling_relation(None, "end", c, "end")
+
+    #se.get_engine("sname").insert_external_event("report", None)
+    se.get_engine("sname").coupling_relation(None, "start", gt, "start")
+    se.get_engine("sname").coupling_relation(None, "end", gt, "end")
 
     # Connect Truck & Can
-    ports = gt.register_garbage_can(i)
-    se.get_engine("sname").coupling_relation(g, "res_garbage", gt, ports[0])
-    se.get_engine("sname").coupling_relation(gt, ports[1], g, "req_empty")
-    i+=1
 
-se.get_engine("sname").insert_input_port("start")
-
-
-se.get_engine("sname").coupling_relation(None, "start", c, "start")
-se.get_engine("sname").coupling_relation(None, "end", c, "end")
-
-#se.get_engine("sname").insert_external_event("report", None)
-se.get_engine("sname").coupling_relation(None, "start", gt, "start")
-se.get_engine("sname").coupling_relation(None, "end", gt, "end")
-
-# Connect Truck & Can
-
-se.get_engine("sname").insert_external_event("start", None)
-se.get_engine("sname").simulate()
+    se.get_engine("sname").insert_external_event("start", None)
+    se.get_engine("sname").simulate()
 
 
--- pohangsim/experiment.py
+++ pohangsim/experiment.py
@@ -1,5 +1,23 @@
 import os
 from config import *
+
+import time
+from functools import wraps
+
+def timefn(fn):
+    @wraps(fn)
+    def measure_time(*args, **kwargs):
+        t1 = time.perf_counter()
+        result = fn(*args, **kwargs)
+        t2 = time.perf_counter()
+        print("@timefn: {} took {} seconds".format(fn.__name__, t2 - t1))
+        return result
+    return measure_time
+
+@timefn
+def execute_fn(executable):
+	os.system(ex)
+
 #532
 exp1 = "python3 exp.py housewife bhs532_N100_seed0  > 532blue_hs_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)+".log"
 exp2 = "python3 exp.py housewife bsh532_N100_seed0  > 532blue_sh_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)+".log"
@@ -19,19 +37,9 @@ exp13 = "python3 exp.py housewife shb721_N100_seed0  > 721stud_hb_"+str(TIME_STD
 exp14 = "python3 exp.py housewife hsb721_N100_seed0  > 721house_sb_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)+".log"
 exp15 = "python3 exp.py housewife hbs721_N100_seed0  > 721house_bs_"+str(TIME_STDDEV)+"trash"+str(TRASH_STDDEV)+str(GARBAGECAN_SIZE)+".log"
 
-os.system(exp1)
-os.system(exp2)
-os.system(exp3)
-os.system(exp4)
-os.system(exp5)
-os.system(exp6)
-os.system(exp7)
-os.system(exp8)
-os.system(exp9)
-os.system(exp10)
-os.system(exp11)
-os.system(exp12)
-os.system(exp13)
-os.system(exp14)
-os.system(exp15)
+ex_list = [exp1, exp2, exp3, exp4, exp5, exp6, exp7, exp8, exp9, exp10, exp11, exp12, exp13, exp14, exp15] 
 
+total = len(ex_list)
+for idx, ex in enumerate(ex_list):
+	print(f"Processing {idx+1}/{total}:", ex)
+	execute_fn(ex)
