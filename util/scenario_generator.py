4f0b56853cdf87e235a5760886d229ec3eb6608b
chlyoo <changhyunlyoo@gmail.com>
2020-05-04 22:02:34 +0900

    class redesign

---
+++ util/scenario_generator.py
@@ -0,0 +1,15 @@
+import contexts
+import math
+
+class Scenario(object):
+	def __init__(self):
+		self.buildings
+		self.student
+		self.homemaker
+		self.construction_worker
+
+class Building(object):
+	def __init__(self):
+
+class Resident(object):
+	def __init__(self):
