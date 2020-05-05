d59a032fa75b086b7e69cb7787eb87548d70e190
chlyoo <changhyunlyoo@gmail.com>
2020-05-05 10:31:34 +0900

    secnario generastor update

--- util/scenario_generator.py
+++ util/scenario_generator.py
@@ -1,6 +1,13 @@
 import contexts
-import math
 
+#familytype and humantype
+from pohangsim.core_component import HumanType
+from pohangsim.core_component import FamilyType
+
+#job class load
+from pohangsim.job import *
+
+print("hello")
 class Scenario(object):
 	def __init__(self):
 		self.buildings
@@ -8,8 +15,17 @@ class Scenario(object):
 		self.homemaker
 		self.construction_worker
 
-class Building(object):
+class Building(Student,Homemaker,Blue_collar,S,H,B):
 	def __init__(self):
+		for _id in range(S):
+			Student(HumanType.__init__(self,_id)).__init__(self,b_id)
 
-class Resident(object):
-	def __init__(self):
+
+class Building(Student,Homemaker,Blue_collar,S,H,B):
+    buidling_id=0
+    def __init__(self, _id, size):
+        self.b_id = buidling_id
+        self.family_id= 0
+        
+        self.garbagecan_size = 50
+    	Stduent(HumanType.__init__(self,_id))
