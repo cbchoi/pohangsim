48293cecd37c5858633bc43f3e7921062846d2c8
chlyoo <changhyunlyoo@gmail.com>
2020-06-04 22:23:07 +0900

    ui update

--- experiments/UI/family_dialog.py
+++ experiments/UI/family_dialog.py
@@ -10,7 +10,7 @@ class FamilyTypeManager(QDialog):
     def __init__(self, _parent=None):
         super(FamilyTypeManager, self).__init__(_parent)
         self.obj = _parent
-        self.setFixedSize(600,200)
+        #self.setFixedSize(600,200)# not working
         print(self.size())
 
     def __getattr__(self, attr):
