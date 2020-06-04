444aec133224b1c4f1190350b248eab334878a4b
chlyoo <changhyunlyoo@gmail.com>
2020-06-04 20:09:59 +0900

    update

--- experiments/UI/building_dialog.py
+++ experiments/UI/building_dialog.py
@@ -1,5 +1,6 @@
 import math
 from PySide2.QtWidgets import *
+from scenario_editor import *
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtCore import QFile, QObject, Slot, Signal, Qt
 from PySide2.QtGui import *
@@ -9,14 +10,36 @@ class BuildingTypeManager(QDialog):
 		super(BuildingTypeManager, self).__init__(_parent)
 		self.obj = _parent
 		self.scenario=scenario
-		self.total_buildingn=len(scenario)
-		self.total_pagen = math.ceil(len(scenario)/6)
+		self.total_buildingn=0
+		self.total_pagen = 0
 		self.currentpage=1 #1로 초기화
+		self.b_in_page = 0
+		#button 동작 정의
 		self.next.clicked.connect(self.increase_page)
 		self.before.clicked.connect(self.decrease_page)
 		self.first.clicked.connect(self.first_page)
 		self.last.clicked.connect(self.last_page)
-		self.b_in_page=0
+		self.addButton.clicked.connect(self.add_building)
+		self.removeButton.clicked.connect(self.remove_buildings)
+		self.update_page()
+
+	def add_building(self):
+		self.scenario.add(BuildingClass())
+		self.update_page()
+	def remove_buildings(self):
+		index_start = 6 * (self.currentpage - 1)
+		if self.B1.isChecked():
+			self.scenario.remove(self.scenario[index_start])
+		if self.B2.isChecked():
+			self.scenario.remove(self.scenario[index_start+1])
+		if self.B3.isChecked():
+			self.scenario.remove(self.scenario[index_start+2])
+		if self.B1_2.isChecked():
+			self.scenario.remove(self.scenario[index_start+3])
+		if self.B2_2.isChecked():
+			self.scenario.remove(self.scenario[index_start+4])
+		if self.B3_2.isChecked():
+			self.scenario.remove(self.scenario[index_start+5])
 		self.update_page()
 	def increase_page(self):
 		if self.currentpage<self.total_pagen:
@@ -34,6 +57,8 @@ class BuildingTypeManager(QDialog):
 		self.update_page()
 
 	def update_page(self):
+		self.total_buildingn = len(self.scenario)
+		self.total_pagen = math.ceil(len(self.scenario) / 6)
 		if self.currentpage==self.total_pagen:
 			self.b_in_page=self.total_buildingn-(6*(self.total_pagen-1))
 		else:
@@ -47,49 +72,52 @@ class BuildingTypeManager(QDialog):
 			showlist.append(flist)
 		index_start =6*(self.currentpage-1)
 		index_end =index_start+self.b_in_page
-		self.update_text(*showlist[index_start:index_end])
-	def update_text(self,B1=False,B2=False,B3=False,B1_2=False,B2_2=False,B3_2=False):
+		self.update_text(index_start,*showlist[index_start:index_end])
+	def update_text(self,id,B1=False,B2=False,B3=False,B1_2=False,B2_2=False,B3_2=False):
 		self.label.setText(str(self.currentpage)+"/"+str(self.total_pagen))
 		B1text,B2text,B3text,B1_2text,B2_2text,B3_2text="","","","","",""
 		if B1:
+			B1text="building id ="+str(id+1)+" \n"
 			for family in B1:
 				B1text+="S:"+str(family.S)+"H:"+str(family.H)+"B:"+str(family.B)+"familycan size:"+str(family.cansize)+"\n"
 			self.B1.setText(B1text)
 		else:
 			self.B1.setVisible(False)
 		if B2:
+			B2text = "building id =" + str(id+2) + "\n"
 			for family in B2:
 				B2text+="S:"+str(family.S)+"H:"+str(family.H)+"B:"+str(family.B)+"familycan size:"+str(family.cansize)+"\n"
 			self.B2.setText(B2text)
 		else:
 			self.B2.setVisible(False)
 		if B3:
+			B3text = "building id =" + str(id+3) + "\n"
 			for family in B3:
 				B3text+="S:"+str(family.S)+"H:"+str(family.H)+"B:"+str(family.B)+"familycan size:"+str(family.cansize)+"\n"
 			self.B3.setText(B3text)
 		else:
 			self.B3.setVisible(False)
-		
 		if B1_2:
+			B1text="building id ="+str(id+4)+" \n"
 			for family in B1_2:
 				B1_2text+="S:"+str(family.S)+"H:"+str(family.H)+"B:"+str(family.B)+"familycan size:"+str(family.cansize)+"\n"
 			self.B1_2.setText(B1_2text)
 		else:
 			self.B1_2.setVisible(False)
 		if B2_2:
+			B2text = "building id =" + str(id+5) + "\n"
 			for family in B2_2:
 				B2_2text+="S:"+str(family.S)+"H:"+str(family.H)+"B:"+str(family.B)+"familycan size:"+str(family.cansize)+"\n"
 			self.B2_2.setText(B2_2text)
 		else:
 			self.B2_2.setVisible(False)
 		if B3_2:
+			B3text = "building id =" + str(id+6) + "\n"
 			for family in B3_2:
 				B3text+="S:"+str(family.S)+"H:"+str(family.H)+"B:"+str(family.B)+"familycan size:"+str(family.cansize)+"\n"
 			self.B3_2.setText(B3_2text)
 		else:
 			self.B3_2.setVisible(False)
-
-
 
 	def __getattr__(self, attr):
 		return getattr(self.obj, attr)
